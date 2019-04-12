75cdc329b4f97384aa1951f1ce36f0200c7951eb
Online Web Tutor <onlinewebtutorhub@gmail.com>
2019-04-12 06:25:46 +0530

    First Program in CI

---
+++ application/views/site/site_about.php
@@ -0,0 +1,3 @@
+<h3>Welcome to About Page</h3>
+<p>This is the playlist of Codeigniter Framework v3.1.10</p>
+
--- application/views/welcome_message.php
+++ application/views/welcome_message.php
@@ -68,7 +68,7 @@ defined('BASEPATH') OR exit('No direct script access allowed');
 <body>
 
 <div id="container">
-	<h1>Welcome to CodeIgniter!</h1>
+	<h1>Welcome to CodeIgniter by Online Web Tutor!</h1>
 
 	<div id="body">
 		<p>The page you are looking at is being generated dynamically by CodeIgniter.</p>
