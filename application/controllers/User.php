032286306eef3e4219aef83d56bb7667d2ece938
Online Web Tutor <onlinewebtutorhub@gmail.com>
2019-04-30 05:50:59 +0530

    Form Validation rules

--- application/controllers/User.php
+++ application/controllers/User.php
@@ -5,22 +5,34 @@ class User extends CI_Controller {
     public function __construct() {
         parent::__construct();
         $this->load->helper(array("form", "url"));
+        $this->load->library("form_validation");
     }
 
     public function form_helper_study() {
 
-        echo base_url();
-        echo "<br/>";
-        echo site_url();
+        //echo base_url();
+        //echo "<br/>";
+        //echo site_url();
         $this->load->view("user/form");
     }
 
     public function form_submit_method() {
-        // we are going to take data from our form
-        $data = $this->input->post();
-        //$data = $this->input->get();
-        echo "<h4>Form data</h4>";
-        echo $data["txt_name"] . " , " . $data["txt_email"];
+
+        $this->form_validation->set_rules("txt_name", "Name", "required");
+
+        $this->form_validation->set_rules("txt_email", "Email", "required");
+
+        if ($this->form_validation->run() == FALSE) {
+            // we have some errors
+            $this->form_helper_study();
+        } else {
+            // submitted form
+            // we are going to take data from our form
+            $data = $this->input->post();
+            //$data = $this->input->get();
+            echo "<h4>Form data</h4>";
+            echo $data["txt_name"] . " , " . $data["txt_email"];
+        }
     }
 
 }
--- application/views/user/form.php
+++ application/views/user/form.php
@@ -1,21 +1,38 @@
 <?php $this->load->view("include/include_form"); ?>
 
+<style>
+    div.error{
+        color: red;
+    }
+</style>
 
 <div class="container">
-    <?php
-    echo form_open(site_url('helpers/form-submit'));
-    ?>
-    <div class="form-group">
-        <label for="txt_name">Name:</label>
-        <input type="text" class="form-control" name="txt_name" id="txt_name" placeholder="Enter name">
-    </div>
-    <div class="form-group">
-        <label for="txt_email">Email:</label>
-        <input type="email" class="form-control" id="email" placeholder="Enter email" name="txt_email">
-    </div>
+    <div class="row">
+        <div class="col-md-6" style="margin:0 auto;">
+            <p>
+                <?php
+                //echo validation_errors();
+                ?>
+            </p>
+            <h3>User Form</h3>
+            <?php
+            echo form_open(site_url('helpers/form-submit'));
+            ?>
+            <div class="form-group">
+                <label for="txt_name">Name:</label>
+                <input type="text" class="form-control" name="txt_name" id="txt_name" placeholder="Enter name">
+                <?php echo form_error("txt_name", "<div class='error'>", "</div>"); ?>
+            </div>
+            <div class="form-group">
+                <label for="txt_email">Email:</label>
+                <input type="email" class="form-control" id="email" placeholder="Enter email" name="txt_email">
+                <?php echo form_error("txt_email", "<div class='error'>", "</div>"); ?>
+            </div>
 
-    <button type="submit" class="btn btn-default">Submit</button>
-    <?php echo form_close(); ?>
+            <button type="submit" class="btn btn-primary">Submit</button>
+            <?php echo form_close(); ?>
+        </div>
+    </div>
 </div>
 
 </body>
